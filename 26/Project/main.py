4b174f0ab96e7d2d87f113ea46b92300af16fcd8
Simone Margio <pipapaperepo@icloud.com>
2022-04-05 15:28:18 +0200

    Day 26
    Improve NATO project with try except

--- 26/Project/main.py
+++ 26/Project/main.py
@@ -1,13 +1,17 @@
 import pandas
 
+program = True
 nato_dict = {}
 nato_alphabet_list = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 for (index, row) in nato_alphabet_list.iterrows():
     nato_dict[row.letter] = row.code
 
-user_word = input("Enter a word:").strip()
-nato_user_word = [nato_dict[letter.upper()] for letter in user_word]
-
-print(nato_user_word)
-
+while program is True:
+    try:
+        user_word = input("Enter a word:").strip()
+        nato_user_word = [nato_dict[letter.upper()] for letter in user_word]
+        program = False
+        print(nato_user_word)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
